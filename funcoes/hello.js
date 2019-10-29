ab0daea43d2598687e44f7280e17296a8fe936c2
Rafaellemos2019 <57117345+Rafaellemos2019@users.noreply.github.com>
2019-10-28 22:28:32 -0300

    backup

---
+++ funcoes/hello.js
@@ -0,0 +1,10 @@
+/**
+ * Exemplo de uma Função Simples
+ * @author Rafael Lemos
+ */
+
+ function helloWorld(){
+     let nome
+     nome = prompt ('Qual é o seu nome?')
+     alert ('Hello ' +  nome)
+ }                          
---
+++ vincularJS/documento.js
@@ -0,0 +1,7 @@
+/**
+ * Exemplo de Vinculação de um documento JS no html
+ * @author Rafael Lemos
+ */
+
+ document.write('<h2>' + 'Hello World' + '</h2>')
+ 
