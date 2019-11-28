d11642b4ec1455c1243cc0b83bac9371e5378029
Rafael Lemos <57117345+Rafaellemos2019@users.noreply.github.com>
2019-11-27 22:28:51 -0300

    AULA 9 jQuery

---
+++ jquery/efeito.js
@@ -0,0 +1,13 @@
+
+
+
+$(document).ready(function () {
+    $("ul.Destaque li img").hover(function () {
+        $("ul.Destaque li img").not(this).stop().fadeTo(400, 0.3)
+
+    }, function () {
+        $("ul.Destaque li img").stop().fadeTo(400, 1)
+
+    })
+})
+
---
+++ jquery/topo.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+$(document).ready(function(){
+    //click
+    $('#top').click(function(){
+        $('html,body').animate({scrollTop: 0}, "fast")
+
+    })
+})
